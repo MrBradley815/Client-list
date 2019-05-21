1d3a5c5788f153ec5a8f96323b0a4962532148bb
Brad Proctor <pb_815@yahoo.com>
2019-05-21 17:14:23 -0500

    Worked on loop
    Not quite right, but closer. Try decrementing loop!

--- client-list.py
+++ client-list.py
@@ -33,21 +33,14 @@ driver.find_element_by_xpath("//ul[@id='side-menu']/li[4]/ul/li[5]/a").click()
 
 wait.until(EC.frame_to_be_available_and_switch_to_it('ContentFrame'))
 
-clients = driver.find_element_by_xpath("//html/body/div[2]/table/tbody/tr[1]")
-while(clients):
-	driver.find_element_by_xpath("//html/body/div[2]/table/tbody/tr[1]/td[6]/input[3]").click()
+x = 4000
+while(x > 1):
+	driver.find_element_by_xpath("//html/body/div[2]/table/tbody/tr[y]/td[6]/input[3]").click()
 	driver.switch_to.alert.accept()
-	wait.until(EC.element_to_be_clickable((By.XPATH, "//input[text()='Delete']")))
-	clients = driver.find_element_by_xpath("//html/body/div[2]/table/tbody/tr[1]")
+
+	wait.until(EC.element_to_be_clickable((By.XPATH, "//html/body/form/input")))
+	x = x - 1
 
 now = time.time()
 
 print("It took: ", (now-then)/60, " minutes")
-
-
-
-
-
-
-
-
